7275c225cf25b8b50d1fb39c9ae37a9c1d87ef43
anas-hfd <anas1.hfd@gmail.com>
2024-01-31 23:07:47 +0100

    caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Caching"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU Cache"""
+    def __init__(self):
+        super().__init__()
+        self.__keys = []
+
+    def put(self, key, item):
+        """put item into cache_data with LIFO algorithm"""
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key in self.cache_data:
+                self.__keys.remove(key)
+            self.__keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value of cache_data dictionary"""
+        if not key or key not in self.cache_data:
+            return None
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Caching"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU Cache """
+    def __init__(self):
+        super().__init__()
+        self.__keys = []
+
+    def put(self, key, item):
+        """put item into cache_data with MRU algorithm"""
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop()
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key not in self.__keys:
+                self.__keys.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value of cache_data dictionary"""
+        if not key or key not in self.cache_data:
+            return None
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
