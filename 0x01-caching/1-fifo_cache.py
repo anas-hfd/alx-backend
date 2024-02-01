53956973359b8a20fa9b0248870c0dabb6377803
anas-hfd <anas1.hfd@gmail.com>
2024-02-01 14:32:37 +0100

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,24 +4,31 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """First In First Out"""
+    """a caching system using the FIFO algorithm
+    """
 
     def __init__(self):
+        """ Initialize
+        """
         super().__init__()
-        self.__keys = []
 
     def put(self, key, item):
-        """put item into cache_data with FIFO algorithm"""
-        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
-            discard = self.__keys.pop(0)
-            del self.cache_data[discard]
-            print('DISCARD: {}'.format(discard))
-        if key and item:
-            self.__keys.append(key)
-            self.cache_data[key] = item
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) == self.MAX_ITEMS:
+            discarded_key = list(self.cache_data.keys())[0]
+            print("DISCARD: {}".format(discarded_key))
+            del self.cache_data[discarded_key]
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        """get value of cache_data dictionary"""
-        if not key or key not in self.cache_data:
+        """ Get an item by key
+        """
+        if key is None:
             return None
-        return self.cache_data[key]
+
+        return self.cache_data.get(key)
