adb4b64e59aebe48eb9f3aeed47c7d15d2f4c2be
anas-hfd <anas1.hfd@gmail.com>
2024-02-01 14:48:11 +0100

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
-"""Caching"""
+"""FIFO caching system."""
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """a caching system using the FIFO algorithm
+    """Frist In, First Out caching system.
+
+    Inherits from BaseCaching and implements FIFO algorithm for caching.
     """
 
     def __init__(self):
-        """ Initialize
+        """Initialize FIFOCache instance.
+
+        Initialize the cache data dictionary and keys list.
         """
         super().__init__()
+        self.__keys = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """Add an item in the cache.
+
+        If the number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS, discard the first item put in cache (FIFO algorithm).
+        You must print 'DISCARD: {key}' with the key discarded and following by a new line.
+
+        :param key: str - key to add to the cache
+        :param item: any - item to add to the cache
         """
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) == self.MAX_ITEMS:
-            discarded_key = list(self.cache_data.keys())[0]
-            print("DISCARD: {}".format(discarded_key))
-            del self.cache_data[discarded_key]
-
-        self.cache_data[key] = item
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            self.__keys.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
-        """
-        if key is None:
-            return None
+        """Get an item by key.
 
-        return self.cache_data.get(key)
+        :param key: str - key to retrieve value from the cache
+        :return: any - value associated with the key in the cache or None if the key is not
+        present in the cache
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
