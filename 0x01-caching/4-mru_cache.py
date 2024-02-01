13493d13cc61317a220eb98309223fb72fdd04cf
anas-hfd <anas1.hfd@gmail.com>
2024-02-01 15:15:32 +0100

    most recently used

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
-"""Caching"""
+"""Caching system using MRU algorithm."""
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRU Cache """
+    """Most Recently Used (MRU) caching system.
+    Inherits from BaseCaching and implements MRU algorithm for caching.
+    Methods:
+        put(key, item): Add an item in the cache with MRU algorithm.
+        get(key): Get an item by key.
+    """
+
     def __init__(self):
+        """Initialize MRUCache instance.
+        Initialize the cache data dictionary and keys list.
+        """
         super().__init__()
         self.__keys = []
 
     def put(self, key, item):
-        """put item into cache_data with MRU algorithm"""
+        """Add an item in the cache with MRU algorithm.
+        If the number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS, discard the most recently used item
+        (MRU algorithm). You must print 'DISCARD: {key}' with the key
+        discarded and following by a new line."""
         if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
             discard = self.__keys.pop()
             del self.cache_data[discard]
@@ -22,7 +35,11 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """get value of cache_data dictionary"""
+        """Get an item by key
+        Returns:
+            any: Value associated with the key in the cache or
+            None if the key is not present in the cache.
+        """
         if not key or key not in self.cache_data:
             return None
         self.__keys.remove(key)
